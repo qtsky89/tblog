4821be686bb5b8fb41d824f2b6bb2af91e897872
Wonhee Jang <wonhee.jang@navercorp.com>
2022-02-01 02:36:14 +0900

    [#1] Added tutorial 2 code (requests and response)

--- tblog_django/tblog_app/urls.py
+++ tblog_django/tblog_app/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from tblog_app import views
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
