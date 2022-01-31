7afef492da1a9dbda1b143bedfce2ec76d17cc66
Wonhee Jang <Qtsky89@gmail.com>
2022-01-31 22:12:08 +0900

    [#1] Minor change

--- tblog-vue/nuxt.config.js
+++ tblog-vue/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'tblog-vue',
+    title: 'tblog_vue',
     htmlAttrs: {
       lang: 'en',
     },
