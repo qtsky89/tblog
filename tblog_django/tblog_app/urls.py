ce5296a3f895d6e48ffe33200b2db3688129692c
Wonhee Jang <wonhee.jang@navercorp.com>
2022-02-01 01:38:07 +0900

    [#1] Added django serialization tutorial code (will be removed)

--- tblog_django/tblog_app/urls.py
+++ tblog_django/tblog_app/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from tblog_app import views
 
 urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
 ]
--- tblog_django/tblog_django/urls.py
+++ tblog_django/tblog_django/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
