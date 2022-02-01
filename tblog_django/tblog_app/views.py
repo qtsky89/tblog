b5b686cd08155c5399914dad85b9660bf11e4dcb
Wonhee Jang <Qtsky89@gmail.com>
2022-02-01 21:06:31 +0900

    [#1] Added tutorial 4 code (authentication & permission)

--- tblog_django/tblog_app/views.py
+++ tblog_django/tblog_app/views.py
@@ -1,13 +1,33 @@
 from tblog_app.models import Snippet
 from tblog_app.serializers import SnippetSerializer
+from tblog_app.serializers import UserSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import permissions
+
+from tblog_app.permissions import IsOwnerOrReadOnly
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
