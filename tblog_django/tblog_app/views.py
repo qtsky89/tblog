c0b369eede6517aa54b97c41a79cc90fd4d8bccb
Wonhee Jang <Qtsky89@gmail.com>
2022-02-02 19:00:59 +0900

    [#5] Added tutorial 5 code (relationships & hyperlinked api)

--- tblog_django/tblog_app/views.py
+++ tblog_django/tblog_app/views.py
@@ -1,11 +1,14 @@
+from rest_framework import renderers
 from tblog_app.models import Snippet
 from tblog_app.serializers import SnippetSerializer
 from tblog_app.serializers import UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
-
 from tblog_app.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -31,3 +34,20 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
