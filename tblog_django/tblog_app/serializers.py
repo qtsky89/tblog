1bb7af4bd53c2941e12abd5163821a97c2d2924f
Wonhee Jang <wonhee.jang@navercorp.com>
2022-02-02 19:00:59 +0900

    [#5] Added tutorial 5 code (relationships & hyperlinked api)

--- tblog_django/tblog_app/serializers.py
+++ tblog_django/tblog_app/serializers.py
@@ -4,16 +4,21 @@ from django.contrib.auth.models import User
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        # fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['url', 'id', 'highlight', 'owner', 'title', 'code', 'linenos', 'language', 'style']
 
 
 class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-    owner = serializers.ReadOnlyField(source='owner.username')
+    # snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+    # owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets', 'owner']
+        # fields = ['id', 'username', 'snippets', 'owner']
+        fields = ['url', 'id', 'username', 'snippets']
