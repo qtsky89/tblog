19e9830de4dd3ab6d8ff60a5642d2283f660e3b6
Wonhee Jang <wonhee.jang@navercorp.com>
2022-02-02 21:00:56 +0900

    [#1] Added tutorial 6 code (viewsets, routers)

--- tblog_django/tblog_app/urls.py
+++ tblog_django/tblog_app/urls.py
@@ -2,15 +2,24 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from tblog_app import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    # path('', views.api_root),
+    path('', include(router.urls)),
+    # path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+    # path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    # path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
 
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    # path('users/', views.UserList.as_view(), name='user-list'),
+    # path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
