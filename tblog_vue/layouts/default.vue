296fcfec0520f775fd8ecc302c807d26ae2d712e
Wonhee Jang <wonhee.jang@navercorp.com>
2022-02-16 22:53:55 +0900

    [#1] Added default layout (navbar)

--- tblog_vue/layouts/default.vue
+++ tblog_vue/layouts/default.vue
@@ -1,3 +1,17 @@
 <template>
-  <Nuxt />
+  <div>
+    <b-navbar class="main-navbar" toggleable="lg" type="light" variant="light">
+      <b-navbar-brand href="#" sticky=true>
+        <h3>Tech Blog</h3>
+      </b-navbar-brand>
+    </b-navbar>
+    <Nuxt />
+  </div>
 </template>
+
+<style scoped>
+.main-navbar {
+  padding-left: 10rem !important;
+  padding-right: 10rem !important;
+}
+</style>
