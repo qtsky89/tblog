5009c88a0978794520dfcaa370c018a22b4692a5
Wonhee Jang <wonhee.jang@navercorp.com>
2022-02-19 23:12:48 +0900

    [#2] Removed DRF code

--- tblog_django/tblog_app/models.py
+++ tblog_django/tblog_app/models.py
@@ -1,39 +1,13 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
-
-from pygments.lexers import get_lexer_by_name
-from pygments.formatters.html import HtmlFormatter
-from pygments import highlight
-
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+from django.utils import timezone
 
 
-class Snippet(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=True, default='')
-    code = models.TextField()
-    linenos = models.BooleanField(default=False)
-    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
-    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+class Post(models.Model):
+    # name = models.CharField(max_length=256, unique=True, primary_key=True)
+    # link = models.CharField(max_length=4096, default='')
+    # image = models.CharField(max_length=4096, default='')
+    # constraint = models.CharField(max_length=256, default='')
+    # created_date = models.DateTimeField(default=timezone.now)
 
-    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE, default='')
-    highlighted = models.TextField(default='')
-
-    def save(self, *args, **kwargs):
-        """
-        Use the `pygments` library to create a highlighted HTML
-        representation of the code snippet.
-        """
-        lexer = get_lexer_by_name(self.language)
-        linenos = 'table' if self.linenos else False
-        options = {'title': self.title} if self.title else {}
-        formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
-        self.highlighted = highlight(self.code, lexer, formatter)
-        super().save(*args, **kwargs)
-
-
-class Meta:
-    ordering = ['created']
+    def __str__(self):
+        return str(self.name)
--- tblog_django/tblog_django/settings.py
+++ tblog_django/tblog_django/settings.py
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'tblog_app',
-    'rest_framework'
+    'tblog_app'
 ]
 
 MIDDLEWARE = [
@@ -147,14 +146,4 @@ LOGGING = {
             'datefmt': '%Y-%m-%d %H:%M:%S %z'
         }
     }
-}
-
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    # ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
 }
