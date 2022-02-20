161d685185a172a471a0ffecbcb233820008617e
Wonhee Jang <Qtsky89@gmail.com>
2022-02-20 19:05:22 +0900

    [#2] Added default CRUD for post.py

--- tblog_django/tblog_app/views/post.py
+++ tblog_django/tblog_app/views/post.py
@@ -1,57 +1,61 @@
 import logging
 import json
 from http import HTTPStatus as Status
-from django.http import JsonResponse
+from django.http import HttpRequest, JsonResponse
 from django.forms.models import model_to_dict
-#from django_app.models import Project
+from tblog_app.models import Post
 from django.views.generic import View
 
 logger = logging.getLogger('django')
 
 
 class ProjectView(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, **kwargs):
         try:
-            return JsonResponse({'message': '', 'data': {}}, status=Status.OK)
+            if kwargs['pk'] == '':
+                data = [model_to_dict(obj) for obj in Post.objects.all().order_by('created_date')]
+            else:
+                data = [model_to_dict(Post.objects.get(pk=kwargs['pk']))]
+
+            return JsonResponse({'message': '', 'data': data}, status=Status.OK)
         except Exception as e:
             logger.error(e, exc_info=True)
-            return JsonResponse({'message': '', 'data': {}}, status=Status.INTERNAL_SERVER_ERROR)
+            return JsonResponse({'message': '', 'data': []}, status=Status.INTERNAL_SERVER_ERROR)
 
-    # def post(self, request, *args, **kwargs):
-    #     try:
-    #         if kwargs['pk'] != '':
-    #             raise Exception('pk should be empty')
+    def post(self, request: HttpRequest, **kwargs):
+        try:
+            if kwargs['pk'] != '':
+                raise Exception('pk should be empty')
 
-    #         data = json.loads(request.body.decode('utf-8'))
-    #         project = Project(**data)
-    #         project.save()
-    #         logger.info(f'{data["name"]} is created with param {data}')
-    #         return JsonResponse({'code': 201, 'message': f'{data["name"]} is created'}, status=201)
-    #     except Exception as e:
-    #         logger.error(e, exc_info=True)
-    #         return JsonResponse({'code': 500, 'message': str(e)}, status=500)
+            data = json.loads(request.body.decode('utf-8'))
+            project = Post(**data)
+            project.save()
+            logger.info(f'post {data["title"]} is created with param {data}')
+            return JsonResponse({'message': f'{data["title"]} is created', 'data': []}, status=Status.CREATED)
+        except Exception as e:
+            logger.error(e, exc_info=True)
+            return JsonResponse({'message': str(e), 'data': []}, status=Status.INTERNAL_SERVER_ERROR)
 
-    # def put(self, request, *args, **kwargs):
-    #     try:
-    #         if kwargs['pk'] == '':
-    #             raise Exception('pk shouldn\'t be empty')
+    def put(self, request: HttpRequest, **kwargs):
+        try:
+            if kwargs['pk'] == '':
+                raise Exception('pk shouldn\'t be empty')
 
-    #         data = json.loads(request.body.decode('utf-8'))
-    #         Project.objects.filter(pk=kwargs['pk']).update(**data)
-    #         logger.info(f'{data["name"]} is updated with param {data}')
-    #         return JsonResponse({'code': 200, 'message': f'{data["name"]} is updated'}, status=200)
+            data = json.loads(request.body.decode('utf-8'))
+            Post.objects.filter(pk=kwargs['pk']).update(**data)
+            logger.info(f'{data["title"]} is replaced with param {data}')
+            return JsonResponse({'message': f'{data["title"]} is replaced'}, status=Status.OK)
+        except Exception as e:
+            logger.error(e, exc_info=True)
+            return JsonResponse({'message': str(e), 'data': []}, status=Status.INTERNAL_SERVER_ERROR)
 
-    #     except Exception as e:
-    #         logger.error(e, exc_info=True)
-    #         return JsonResponse({'code': 500, 'message': str(e)}, status=500)
-
-    # def delete(self, request, *args, **kwargs):
-    #     try:
-    #         if kwargs['pk'] == '':
-    #             raise Exception('pk shouldn\'t be empty')
-    #         Project.objects.filter(pk=kwargs['pk']).delete()
-    #         logger.info(f'{kwargs["pk"]} is deleted')
-    #         return JsonResponse({'code': 200, 'message': f'{kwargs["pk"]} is deleted'}, status=200)
-    #     except Exception as e:
-    #         logger.error(e, exc_info=True)
-    #         return JsonResponse({'code': 500, 'message': str(e)}, status=500)
+    def delete(self, **kwargs):
+        try:
+            if kwargs['pk'] == '':
+                raise Exception('pk shouldn\'t be empty')
+            Post.objects.filter(pk=kwargs['pk']).delete()
+            logger.info(f'{kwargs["pk"]} is deleted')
+            return JsonResponse({'message': f'{kwargs["pk"]} is deleted'}, status=Status.OK)
+        except Exception as e:
+            logger.error(e, exc_info=True)
+            return JsonResponse({'message': str(e), 'data': []}, status=Status.INTERNAL_SERVER_ERROR)
