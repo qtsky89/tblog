fdac62e9d5270f49d7bf3128cad323b46e35aefe
Wonhee Jang <wonhee.jang@navercorp.com>
2022-03-06 20:54:17 +0900

    [#2] Modified css for main page

--- tblog_vue/layouts/default.vue
+++ tblog_vue/layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <b-navbar class="main-navbar" toggleable="lg" type="light" variant="light">
-      <b-navbar-brand sticky=true>
-        <NuxtLink to="/">Home page</NuxtLink>
-      </b-navbar-brand>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <div class="main-navbar">
+        <b-navbar-brand sticky=true>
+          <NuxtLink class="main-link" to="/">Tblog</NuxtLink>
+        </b-navbar-brand>
+      </div>
     </b-navbar>
     <Nuxt />
   </div>
@@ -11,7 +13,15 @@
 
 <style scoped>
 .main-navbar {
-  padding-left: 10rem !important;
-  padding-right: 10rem !important;
+  width: 70%;
+  vertical-align: middle;
+  margin: 0 auto;
+}
+
+.main-link {
+  color: black;
+  font-weight: bold;
+  font-size: 1.5rem;
+  font-family: "Quicksand", sans-serif;
 }
 </style>
