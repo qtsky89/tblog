64372bbf49bde8ad8b6751a8d0933b6c0e17a80f
Wonhee Jang <Qtsky89@gmail.com>
2022-03-22 22:15:57 +0900

    [#2] Added tag model

--- tblog_django/tblog_app/models.py
+++ tblog_django/tblog_app/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from django.utils import timezone
 
 
+class Tag(models.Model):
+    name = models.CharField(primary_key=True, max_length=255)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
 class Post(models.Model):
     id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=4096, default='')
     body = models.TextField(default='')
     created_date = models.DateTimeField(default=timezone.now)
-    tag = models.ManyToManyField('Tag')
+    tag = models.ManyToManyField(Tag)
 
     def __str__(self) -> str:
         return str(self.id)
-
-
-class Tag(models.Model):
-    name = models.CharField(primary_key=True, max_length=256)
-
-    def __str__(self) -> str:
-        return str(self.name)
