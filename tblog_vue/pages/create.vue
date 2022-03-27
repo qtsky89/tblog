0f1874627f851e65ad20ca3948d539c0e692fd65
Wonhee Jang <Qtsky89@gmail.com>
2022-03-27 19:10:12 +0900

    [#6] Minor change

--- tblog_vue/pages/create.vue
+++ tblog_vue/pages/create.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
+    <b-form-input v-model="test" placeholder="Enter your name"></b-form-input>
   </div>
 </template>
 
 <script lang="ts">
 export default {
   data() {
-    text: ''
+    return {
+      test: '',
+    }
   },
 }
 </script>
