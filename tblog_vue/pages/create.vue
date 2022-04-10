e91246d21b5aca209968349c4b6ad487022b2994
Wonhee Jang <Qtsky89@gmail.com>
2022-04-10 15:00:07 +0900

    [#6] Added mavon editor

--- tblog_vue/pages/create.vue
+++ tblog_vue/pages/create.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <b-form-input v-model="test" placeholder="Enter your name"></b-form-input>
+  <div class="total">
+    <b-container class="editor-container">
+      <b-row>
+        <no-ssr>
+          <mavon-editor
+            v-model="editorData"
+            class="editor"
+            :toolbars="toolbarOption"
+            language="en"
+            font-size="15px"
+            placeholder="Please enter the post body"
+          />
+        </no-ssr>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -8,10 +21,51 @@
 export default {
   data() {
     return {
-      test: '',
+      title: '',
+      toolbarOption: {
+        bold: true,
+        italic: true,
+        header: true,
+        underline: true,
+        strikethrough: true,
+        mark: true,
+        superscript: true,
+        subscript: true,
+        quote: true,
+        ol: true,
+        ul: true,
+        link: true,
+        imagelink: true,
+        code: true,
+        table: true,
+        fullscreen: true,
+        readmodel: true,
+        htmlcode: true,
+        help: true,
+        undo: true,
+        redo: true,
+        trash: true,
+        save: true,
+        navigation: true,
+        alignleft: true,
+        aligncenter: true,
+        alignright: true,
+        subfield: true,
+        preview: true,
+      },
+      editorData: '',
     }
   },
 }
 </script>
 
-<style></style>
+<style scoped>
+.editor-container {
+  width: 100%;
+}
+
+.editor {
+  width: 100%;
+  height: 100%;
+}
+</style>
