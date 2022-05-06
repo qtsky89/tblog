c7db2fc075d80719e3220a553a26a3a6d00377d7
Wonhee Jang <Qtsky89@gmail.com>
2022-05-06 18:32:29 +0900

    [#13] Added control avatar

--- tblog_quasar/src/layouts/MainLayout.vue
+++ tblog_quasar/src/layouts/MainLayout.vue
@@ -10,22 +10,69 @@
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
-        <q-toolbar-title>Wonhee's Tech Blog</q-toolbar-title>
+        <q-toolbar-title>
+          <router-link class="main-link" to="/">Wonhee's Tech Blog</router-link>
+        </q-toolbar-title>
 
-        <!-- TODO: add control feature (create, update, login, logout)-->
-        <q-avatar class="bg-black">
-          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-        </q-avatar>
+        <!-- TODO: add control feature (create, update, delete, login, logout)-->
+        <q-btn-dropdown class="q-btn--flat">
+          <template #label>
+            <q-avatar>
+              <img
+                class="bg-black"
+                src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg"
+              />
+            </q-avatar>
+          </template>
+
+          <q-list>
+            <q-item clickable v-close-popup to="/create">
+              <q-item-section avatar>
+                <q-avatar
+                  size="25px"
+                  icon="add"
+                  color="blue-10"
+                  text-color="white"
+                />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Create</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-close-popup to="/update">
+              <q-item-section avatar>
+                <q-avatar
+                  size="25px"
+                  icon="edit"
+                  color="green-10"
+                  text-color="white"
+                />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Update</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+
+          <q-item clickable v-close-popup to="/delete">
+            <q-item-section avatar>
+              <q-avatar
+                size="25px"
+                icon="add"
+                color="red-10"
+                text-color="white"
+              />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Delete</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-btn-dropdown>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      side="left"
-      show-if-above
-      bordered
-      overlay
-    >
+    <q-drawer v-model="leftDrawerOpen" side="left" bordered overlay>
       <q-list>
         <EssentialLink
           v-for="link in essentialLinks"
@@ -53,8 +100,18 @@ const essentialLinks = [
     link: 'https://quasar.dev',
   },
 ]
-let leftDrawerOpen = ref(false)
+const leftDrawerOpen = ref(false)
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
 </script>
+
+<style scoped>
+.main-link {
+  color: black;
+  font-weight: bold;
+  font-size: 1.5rem;
+  font-family: 'Quicksand', sans-serif;
+  text-decoration: none;
+}
+</style>
