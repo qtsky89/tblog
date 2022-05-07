0282713510e90452e70f9a661ee415a92a5078f2
Wonhee Jang <Qtsky89@gmail.com>
2022-05-07 20:39:05 +0900

    [#13] Refactoring control dropdown

--- tblog_quasar/src/layouts/MainLayout.vue
+++ tblog_quasar/src/layouts/MainLayout.vue
@@ -13,62 +13,7 @@
         <q-toolbar-title>
           <router-link class="main-link" to="/">Wonhee's Tech Blog</router-link>
         </q-toolbar-title>
-
-        <!-- TODO: add control feature (create, update, delete, login, logout)-->
-        <q-btn-dropdown class="q-btn--flat">
-          <template #label>
-            <q-avatar>
-              <img
-                class="bg-black"
-                src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg"
-              />
-            </q-avatar>
-          </template>
-
-          <q-list>
-            <q-item clickable v-close-popup to="/create">
-              <q-item-section avatar>
-                <q-avatar
-                  size="25px"
-                  icon="add"
-                  color="blue-10"
-                  text-color="white"
-                />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Create</q-item-label>
-              </q-item-section>
-            </q-item>
-
-            <q-item clickable v-close-popup to="/update">
-              <q-item-section avatar>
-                <q-avatar
-                  size="25px"
-                  icon="edit"
-                  color="green-10"
-                  text-color="white"
-                />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Update</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-
-          <q-item clickable v-close-popup to="/delete">
-            <q-item-section avatar>
-              <q-avatar
-                size="25px"
-                icon="add"
-                color="red-10"
-                text-color="white"
-              />
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>Delete</q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-btn-dropdown>
+        <ControlDropdown />
       </q-toolbar>
     </q-header>
 
@@ -91,6 +36,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import EssentialLink from 'components/EssentialLink.vue'
+import ControlDropdown from 'components/ControlDropdown.vue'
 
 const essentialLinks = [
   {
