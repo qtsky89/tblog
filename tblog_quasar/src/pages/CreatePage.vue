3ce1856b2acd69afb57d993b4f77a43d4aa9229f
Wonhee Jang <Qtsky89@gmail.com>
2022-05-15 19:29:25 +0900

    [#13] Added router sample code in CreatePage.vue

--- tblog_quasar/src/pages/CreatePage.vue
+++ tblog_quasar/src/pages/CreatePage.vue
@@ -1,3 +1,13 @@
 <template>
   <p>create page</p>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+onMounted(() => {
+  const route = useRoute()
+  console.log(route.params)
+})
+</script>
