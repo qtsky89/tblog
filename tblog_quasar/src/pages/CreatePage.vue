eeb41a8a3d572af89c5255d75b62e0c708917067
Wonhee Jang <Qtsky89@gmail.com>
2022-05-16 17:43:44 +0900

    [#13] Added mavon editor

--- tblog_quasar/src/pages/CreatePage.vue
+++ tblog_quasar/src/pages/CreatePage.vue
@@ -1,13 +1,100 @@
 <template>
-  <p>create page</p>
+  <q-page class="q-pa-md">
+    <div class="editor-container">
+      <q-input class="input-form" v-model="data.title" label="title">
+        <template v-slot:after>
+          <q-btn round @click="publish" icon="send" />
+        </template>
+      </q-input>
+      <q-input
+        class="input-form"
+        v-model="data.description"
+        label="description"
+      />
+      <!-- TODO: add tag form -->
+      <q-no-ssr>
+        <mavon-editor
+          v-model="data.body"
+          class="editor q-mt-lg"
+          :toolbars="editorOption"
+          language="en"
+          font-size="16px"
+          :box-shadow="true"
+        />
+      </q-no-ssr>
+    </div>
+  </q-page>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, reactive } from 'vue'
 import { useRoute } from 'vue-router'
+import { api } from 'boot/axios'
 
 onMounted(() => {
   const route = useRoute()
   console.log(route.params)
 })
+
+const data = reactive({
+  title: '',
+  body: '',
+  description: '',
+  tags: [],
+})
+
+const editorOption = {
+  bold: true,
+  italic: true,
+  header: true,
+  underline: true,
+  strikethrough: true,
+  mark: true,
+  superscript: true,
+  subscript: true,
+  quote: true,
+  ol: true,
+  ul: true,
+  link: true,
+  imagelink: true,
+  code: true,
+  table: true,
+  fullscreen: true,
+  readmodel: true,
+  htmlcode: true,
+  help: true,
+  undo: true,
+  redo: true,
+  trash: true,
+  navigation: true,
+  alignleft: true,
+  aligncenter: true,
+  alignright: true,
+  subfield: true,
+  preview: true,
+}
+
+async function publish() {
+  try {
+    await api.post('/api/v1/post', data)
+  } catch (error) {
+    console.error(error)
+  }
+}
 </script>
+
+<style scoped>
+.input-form {
+  width: 100%;
+}
+.editor-container {
+  width: 1000px;
+  margin: auto;
+}
+
+.editor {
+  width: 100%;
+  height: 65vh;
+  /* border: 1px solid #ced4da; */
+}
+</style>
--- tblog_quasar/src/router/routes.ts
+++ tblog_quasar/src/router/routes.ts
@@ -11,6 +11,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/CreatePage.vue') }],
   },
+  {
+    path: '/update/:id',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/CreatePage.vue') }],
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
